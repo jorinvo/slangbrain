169a3e765e14c0f5d3286b29b02d816c5b17a599
Jorin Vogel <hi@jorin.me>
2016-09-20 22:41:31 +0200

    store in sqlite and added query matching

---
+++ brain/brain.go
@@ -0,0 +1,125 @@
+package brain
+
+import (
+	"database/sql"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	setupStmt = `
+		CREATE TABLE IF NOT EXISTS facts (
+			id INTEGER PRIMARY KEY,
+			chatid BIGINT,
+			content TEXT
+		);
+		CREATE TABLE IF NOT EXISTS chats (
+			id INTEGER PRIMARY KEY,
+			chatid BIGINT,
+			userid INTEGER
+		);
+		CREATE UNIQUE INDEX IF NOT EXISTS uniqidcombi ON chats(chatid, userid);
+	`
+
+	addChatStmt = "REPLACE INTO chats (userid, chatid) VALUES ($1, $2)"
+
+	addFactStmt = "INSERT INTO facts (chatid, content) VALUES ($1, $2)"
+
+	findFactsStmt = `
+		SELECT id, chatid, content FROM facts
+		WHERE chatid IN (SELECT chatid FROM chats WHERE userid = $1)
+		AND content LIKE '%' || $2 || '%'
+	`
+)
+
+// Fact is one element a user saved.
+// It also matches a row in the facts table.
+//
+// Currently a fact just contains a single text content.
+// In the future there might be different fields with different functions.
+//
+// A fact belongs to a ChatID.
+// This way users can share knowledge using a group chat
+// or a single user can create multiple groups as "studying groups" to separate different kinds of facts.
+type Fact struct {
+	ID      int
+	ChatID  int
+	Content string
+}
+
+// Store is the interface to an underlying database.
+// Keeping it separated allows to switch the database implementation.
+type Store interface {
+	AddChat(int, int64) error
+	AddFact(int64, string) error
+	FindFacts(int, string) ([]Fact, error)
+}
+
+// SQLStore implements a Store as classical SQL database.
+// It uses the standard database/sql package and supports different backends.
+// Current implementation is tested with SQLite.
+type SQLStore struct {
+	db *sql.DB
+}
+
+// CreateStore returns a new SQLStore with a database already setup.
+// It forwards the arguments to the standard database/sql package.
+func CreateStore(driverName, dataSourceName string) (SQLStore, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	store := SQLStore{db}
+	if err != nil {
+		return store, errors.Wrap(err, "failed to initialize database adapter")
+	}
+
+	_, err = db.Exec(setupStmt)
+	if err != nil {
+		return store, errors.Wrap(err, "failed to setup tables")
+	}
+
+	err = db.Ping()
+	return store, errors.Wrap(err, "failed to connect to database")
+}
+
+// AddChat saves a relation between a userID and chatID.
+// Relations are stored unique - even after repeated calling of AddChat.
+func (store SQLStore) AddChat(userID int, chatID int64) error {
+	_, err := store.db.Exec(addChatStmt, userID, chatID)
+	return errors.Wrapf(err, "failed to add chat for userID %d and chatID %d", userID, chatID)
+}
+
+// AddFact stores a new fact.
+func (store SQLStore) AddFact(chatID int64, text string) error {
+	_, err := store.db.Exec(addFactStmt, chatID, text)
+	return errors.Wrapf(err, "failed to add fact for chatID %d: %s", chatID, text)
+}
+
+// FindFacts returns all facts that contain the query string.
+// It only matches the query as a whole, but the position of the query in a fact doesn't matter.
+// It searches all chats a user is part of.
+func (store SQLStore) FindFacts(userID int, query string) (facts []Fact, err error) {
+	rows, err := store.db.Query(findFactsStmt, userID, query)
+	if err != nil {
+		return facts, errors.Wrapf(err, "failed to query facts for userID %d and query '%s'", userID, query)
+	}
+	defer func() {
+		closeErr := rows.Close()
+		if closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}()
+	for rows.Next() {
+		var f Fact
+		err = rows.Scan(&f.ID, &f.ChatID, &f.Content)
+		if err != nil {
+			return facts, errors.Wrap(err, "failed to scan row")
+		}
+		facts = append(facts, f)
+	}
+	err = rows.Err()
+	return facts, errors.Wrap(err, "failed while querying facts")
+}
+
+// Close the underlying database connection.
+func (store *SQLStore) Close() error {
+	return errors.Wrap(store.db.Close(), "failed to close database")
+}
--- main.go
+++ main.go
@@ -5,42 +5,66 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
+	"./brain"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 	tg "gopkg.in/telegram-bot-api.v4"
 )
 
-type chatBrains map[int64][]string
-type int64Set map[int64]bool
-type userChats map[int]int64Set
+var (
+	startCmd = "/start"
+	helpCmd  = "/help"
+	addCmd   = "/add"
 
-var welcomeMsg = `Hello %s!
-I will help you study your own way.
+	// Emoji :pencil2:
+	addButton = addCmd + " \xE2\x9C\x8F"
+
+	// Markdown formatted
+	helpReply = `Hello %s!
+I help you study your own way.
 You teach me some facts.
 I help you to remember them.
 
-Start by adding a new fact using the button below.`
+Start by adding a new fact using the __/add__ button below.
 
-var startCmd = "/start"
-var addCmd = "/add"
+After you added some facts type  _@slngbot ..._ to search them.`
 
-// Emoji :pencil2:
-var addButton = addCmd + " \xE2\x9C\x8F"
+	// Emoji :memo:
+	addReply = "Send me a new fact \xF0\x9F\x93\x9D"
 
-// Emoji :memo:
-var addReply = "Send me a new fact \xF0\x9F\x93\x9D"
+	// Emoji :scream:
+	errReply = "Something went wrong, try again \xF0\x9F\x98\xB1"
 
-// Emoji :scream:
-var defaultReply = "Something went wrong, try again \xF0\x9F\x98\xB1"
+	// Emoji :+1:
+	addDoneReply = "Fact added \xF0\x9F\x91\x8D"
 
-// Emoji :+1:
-var addDoneReply = "Fact added \xF0\x9F\x91\x8D"
+	// Markdown formatted
+	inlineQueryReply = `_%s_
+
+%s`
+)
 
 func main() {
-	bot, err := tg.NewBotAPI("241302379:AAH6qAM-bnqp81zR2pl_PQePHec-FpYwRBI")
+	store, err := brain.CreateStore("sqlite3", "./slangbrain.db")
 	if err != nil {
 		log.Panic(err)
 	}
+
+	bot, err := tg.NewBotAPI("241302379:AAH6qAM-bnqp81zR2pl_PQePHec-FpYwRBI")
+	if err != nil {
+		defer func() {
+			err := store.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}()
+		log.Panic(err)
+	}
+
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tg.NewUpdate(0)
@@ -48,47 +72,92 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
-	var brains = make(chatBrains)
-	var chats = make(userChats)
-
 	for update := range updates {
-		handleMessage(bot, update.Message, brains, chats)
-		handleInlineQuery(bot, update.InlineQuery, brains, chats)
+		handleMessage(store, bot, update.Message)
+		handleInlineQuery(store, bot, update.InlineQuery)
 	}
 }
 
-func handleMessage(bot *tg.BotAPI, msg *tg.Message, brains chatBrains, chats userChats) {
+func handleMessage(store brain.Store, bot *tg.BotAPI, msg *tg.Message) {
 	if msg == nil {
 		return
 	}
 
 	userID := msg.From.ID
 	chatID := msg.Chat.ID
-	text := msg.Text
-	replyToMessage := msg.ReplyToMessage
 
-	chats.add(userID, chatID)
+	log.Printf("  msg: [%s] %s", msg.From.UserName, msg.Text)
 
 	var reply tg.MessageConfig
-	switch {
-	case strings.HasPrefix(text, startCmd):
-		reply = addKeyboard(tg.NewMessage(chatID, fmt.Sprintf(welcomeMsg, msg.From.FirstName)))
-	case strings.HasPrefix(text, addCmd):
-		reply = forceReply(tg.NewMessage(chatID, addReply))
-	case replyToMessage != nil && replyToMessage.Text == addReply:
-		brains[chatID] = append(brains[chatID], text)
-		reply = addKeyboard(tg.NewMessage(chatID, addDoneReply))
-		reply.ReplyToMessageID = msg.MessageID
-	default:
-		reply = addKeyboard(tg.NewMessage(chatID, defaultReply))
+
+	err := store.AddChat(userID, chatID)
+	if err != nil {
+		log.Println(errors.Wrap(err, "failed to save chat"))
+		reply = addKeyboard(tg.NewMessage(chatID, errReply))
+	} else {
+		switch {
+		// `/start` or `/help`
+		case strings.HasPrefix(msg.Text, startCmd) || strings.HasPrefix(msg.Text, helpCmd):
+			reply = addKeyboard(tg.NewMessage(chatID, fmt.Sprintf(helpReply, msg.From.FirstName)))
+			reply.ParseMode = "Markdown"
+		// `/add`
+		case strings.HasPrefix(msg.Text, addCmd):
+			reply = forceReply(tg.NewMessage(chatID, addReply))
+		// `/add` reply
+		case msg.ReplyToMessage != nil && msg.ReplyToMessage.Text == addReply:
+			err := store.AddFact(chatID, msg.Text)
+			if err != nil {
+				log.Println(err)
+				reply = addKeyboard(tg.NewMessage(chatID, errReply))
+			} else {
+				reply = addKeyboard(tg.NewMessage(chatID, addDoneReply))
+				reply.ReplyToMessageID = msg.MessageID
+			}
+		}
 	}
 
-	log.Printf("  msg: [%s] %s", msg.From.UserName, text)
+	// Ignore unhandled messages
+	if reply.Text == "" {
+		return
+	}
 
-	_, err := bot.Send(reply)
-
+	_, err = bot.Send(reply)
 	if err != nil {
-		log.Print(err)
+		log.Println(errors.Wrap(err, "failed sending reply"))
+	}
+}
+
+func handleInlineQuery(store brain.Store, bot *tg.BotAPI, q *tg.InlineQuery) {
+	if q == nil {
+		return
+	}
+
+	log.Printf("query: [%s] %s", q.From.UserName, q.Query)
+
+	userID := q.From.ID
+	var results []interface{}
+	facts, err := store.FindFacts(userID, q.Query)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// search in all brains of user
+	for i, fact := range facts {
+		title := fmt.Sprintf("%v %s", i+1, firstChars(10, fact.Content))
+		msg := fmt.Sprintf(inlineQueryReply, q.Query, fact.Content)
+		result := tg.NewInlineQueryResultArticleMarkdown(strconv.Itoa(fact.ID), title, msg)
+		result.Description = fact.Content
+		results = append(results, result)
+	}
+
+	inlineConf := tg.InlineConfig{
+		InlineQueryID: q.ID,
+		IsPersonal:    true,
+		Results:       results,
+	}
+
+	if _, err := bot.AnswerInlineQuery(inlineConf); err != nil {
+		log.Println(err)
 	}
 }
 
@@ -104,39 +173,9 @@ func forceReply(msg tg.MessageConfig) tg.MessageConfig {
 	return msg
 }
 
-func (chats userChats) add(userID int, chatID int64) {
-	if chats[userID] == nil {
-		chats[userID] = make(int64Set)
+func firstChars(i int, s string) string {
+	if i > len(s) {
+		return s[0:len(s)]
 	}
-	chats[userID][chatID] = true
-}
-
-func handleInlineQuery(bot *tg.BotAPI, q *tg.InlineQuery, brains chatBrains, chats userChats) {
-	if q == nil {
-		return
-	}
-
-	log.Printf("query: [%s] %s", q.From.UserName, q.Query)
-
-	userID := q.From.ID
-	var results []interface{}
-	// search in all brains of user
-	for chatID := range chats[userID] {
-		for i, fact := range brains[chatID] {
-			// query id is used by Telegram for caching
-			result := tg.NewInlineQueryResultArticle(fmt.Sprint(userID, chatID, i), fmt.Sprintf("Fact %v", i), fact)
-			result.Description = fact
-			results = append(results, result)
-		}
-	}
-
-	inlineConf := tg.InlineConfig{
-		InlineQueryID: q.ID,
-		IsPersonal:    true,
-		Results:       results,
-	}
-
-	if _, err := bot.AnswerInlineQuery(inlineConf); err != nil {
-		log.Println(err)
-	}
+	return s[0:i] + "..."
 }
