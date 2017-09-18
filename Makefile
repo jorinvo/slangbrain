4fa426c593a2df3e8d55aa4cb34d9aba08f75106
Jorin Vogel <hi@jorin.me>
2017-09-18 14:42:24 +0200

    Run slangbrain as production server
    Run http on port 80 and https on port 443.
    Move all scripts to Makefile.
    Track systemd service file in Git.
    Setup security headers, http timeouts, gzip and enforce https.

---
+++ Makefile
@@ -0,0 +1,99 @@
+PROD := fbot.slangbrain.com
+PROD_DB := $(PROD):/etc/slangbrain/slangbrain.db
+PROD_BIN := /usr/local/bin/slangbrain
+DEV := tunnel.slangbrain.com
+
+STAT := stat.slangbrain.com
+STAT_BIN := slangbrain-stat
+STAT_FILE := /usr/local/bin/$(STAT_BIN)
+
+VERSION := $(shell go version | cut -d' ' -f3)_commit_$(shell git log --format="%H" -n 1)
+
+MIGRATION := $(shell ls -1 migrations | tail -n1)
+MIGRATION_FILE := ./migrations/$(MIGRATION)/main.go
+NUM := $(shell echo "$(MIGRATION)" | cut -d_ -f1)
+BEFORE_DB := backups/$(NUM)-before.db
+AFTER_DB := backups/$(NUM)-after.db
+
+FB_TOKEN := EAAEMXBS5vNoBAB4NbuAYJp1FhDN50UNcoFRtME4phWQEGdV3ezUUkZCVS6B1Q2vQHFPc4TUZBdMTwEWjkwzfFNR2WR5cYDxjXZCZCWKUgAlZBewOGKZB1Un2gSbaBKV2L4bgn7vR5ZC83Lo7kd53WZAimctkkwRzBmzA1UYRvR0sgQZDZD
+FB_SECRET := 414a6cdf4c8da5bbb281960cfcfe3eeb
+VERIFY_TOKEN := SmhklHbrVi4MInnC8Fih58TBTIc$$jeXTadn%bChS
+
+SLACK_HOOK := https://hooks.slack.com/services/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
+
+
+
+# Run dev server locally
+run:
+	@go run main.go \
+		-db 'dev.db' \
+		-http 8080 \
+		-domain $(DEV) \
+		-verify $(VERIFY_TOKEN) \
+		-token $(FB_TOKEN) \
+		-secret $(FB_SECRET) \
+		-slackhook $(SLACK_HOOK) \
+		$@
+
+.PHONY: run
+
+
+
+# Run integration tests
+test:
+	@go test -v \
+		-coverpkg ./api,./brain,./common,./fbot,./messenger,./payload,./slack,./translate,./user \
+		./integration
+
+.PHONY: test
+
+
+
+# Build, and deploy latest version of Slangbrain to the live server
+deploy:
+	GOOS=linux go build -a -ldflags "-s -w -X main.version=$(VERSION)" -o dist/slangbrain
+	scp dist/slangbrain $(PROD):/tmp/slangbrain
+	@echo "switch to new binary and restart service"
+	@ssh -t $(PROD) "sh -c 'sudo mv $(PROD_BIN) /tmp/slangbrain-$(shell date +%s)' && sudo mv /tmp/slangbrain $(PROD_BIN) && sudo systemctl restart slangbrain && sudo journalctl -fu slangbrain"
+
+.PHONY: deploy
+
+
+# Deploy slangbrain-stat binary
+deploy-stat:
+	GOOS=linux go build -a -ldflags "-s -w" -o dist/$(STAT_BIN) ./cmd/$(STAT_BIN)/main.go
+	ssh $(STAT) mv $(STAT_FILE) /tmp/$(STAT_BIN)-$(shell date +%s)
+	scp dist/$(STAT_BIN) $(STAT):$(STAT_FILE)
+
+.PHONY: deploy-stat
+
+
+
+# Apply most recent migration on production server
+migrate:
+	@mkdir -p backups
+
+	errcheck $(MIGRATION_FILE)
+
+	ssh -t $(PROD) sudo systemctl stop slangbrain
+
+	scp $(PROD_DB) $(BEFORE_DB)
+	cp $(BEFORE_DB) $(AFTER_DB)
+
+	go run $(MIGRATION_FILE) $(AFTER_DB)
+	@sleep 2
+
+	scp $(AFTER_DB) $(PROD_DB)
+	deploy
+
+.PHONY: migrate
+
+
+
+# Rollback data to before the last migration
+rollback:
+	ssh -t $(PROD) sudo systemctl stop slangbrain
+	scp backups/$(shell echo "$(MIGRATION)" | cut -d_ -f1)-before.db $(PROD_DB)
+	ssh -t $(PROD) "sh -c 'sudo systemctl restart slangbrain && sudo journalctl -fu slangbrain'"
+
+.PHONY: rollback
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
@@ -11,12 +12,14 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/NYTimes/gziphandler"
 	"github.com/jorinvo/slangbrain/api"
 	"github.com/jorinvo/slangbrain/brain"
 	"github.com/jorinvo/slangbrain/messenger"
 	"github.com/jorinvo/slangbrain/messenger/webview"
 	"github.com/jorinvo/slangbrain/slack"
 	"github.com/jorinvo/slangbrain/translate"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 const cliUsage = `Slangbrain Messenger bot
@@ -28,8 +31,8 @@ Data is stored in a single file. No external system is needed.
 However, only one application can access the database at a time.
 
 Slangbrain starts a server to serve a webhook handler at /webhook that can be registered as a Messenger bot.
-The server is HTTP only and a proxy server should be used to make the bot available on
-a public domain, preferably HTTPS only.
+If -http PORT is passed an HTTP-only server is started. Otherwise a production server listening on :80 and :443 is started,
+redirecting all traffic to HTTPS. Let's Encrypt is used for automatic certificate loading.
 
 Certain features are better done with a custom web view.
 They are rendered at /webview.
@@ -43,6 +46,12 @@ and admin replies in Slack are send back to the users.
 Flags:
 `
 
+const (
+	readTimeout  = 5 * time.Second
+	writeTimeout = 10 * time.Second
+	idleTimeout  = 120 * time.Second
+)
+
 var version = "development"
 
 func main() {
@@ -51,14 +60,16 @@ func main() {
 
 	versionFlag := flag.Bool("version", false, "Print the version of the binary.")
 	db := flag.String("db", "", "Required. Path to BoltDB file. Will be created if non-existent.")
-	port := flag.Int("port", 8080, "Port server listens on.")
+	httpPort := flag.Int("http", -1, "Address http server listens on. If given, runs http only. If empty runs https on 443 and redirects http on 80.")
+	email := flag.String("email", "", "Requrired unless -http. Email address to use as contact for Let's Encrypt.")
+	certCache := flag.String("certdir", "", "Requrired unless -http. Directory to cache certificates.")
 	verifyToken := flag.String("verify", "", "Required. Messenger bot verify token.")
 	token := flag.String("token", "", "Required. Messenger bot token.")
 	secret := flag.String("secret", "", "Required. Facebook app secret.")
 	slackHook := flag.String("slackhook", "", "Required. URL of Slack Incoming Webhook. Used to send user messages to admin.")
 	slackToken := flag.String("slacktoken", "", "Token for Slack Outgoing Webhook. Used to send admin answers to user messages.")
 	backupAuth := flag.String("backupauth", "", "/backup basic auth in the form user:pasword. If empty, /backup is deactivated.")
-	baseURL := flag.String("url", "https://fbot.slangbrain.com", "Overwrite the base URL. Used for linking webviews.")
+	domain := flag.String("domain", "fbot.slangbrain.com", "Domain used for certs and internal links.")
 	noSetup := flag.Bool("nosetup", false, "Skip sending setup instructions to Facebook")
 
 	// Parse and validate flags
@@ -71,6 +82,15 @@ func main() {
 	if *versionFlag {
 		infoLogger.Println("Slangbrain", version)
 		os.Exit(0)
+	}
+
+	if *httpPort == 0 {
+		if *email == "" {
+			errorLogger.Fatalln("Flag -email is required")
+		}
+		if *certCache == "" {
+			errorLogger.Fatalln("Flag -certdir is required")
+		}
 	}
 
 	if *db == "" {
@@ -106,7 +126,7 @@ func main() {
 	}()
 	infoLogger.Printf("Database initialized: %s", *db)
 
-	translator := translate.New(*baseURL)
+	translator := translate.New("https://" + *domain)
 
 	// Listen to system events for graceful shutdown
 	shutdownSignals := make(chan os.Signal, 1)
@@ -177,22 +197,79 @@ func main() {
 	if *backupAuth != "" {
 		mux.Handle("/backup", backupHandler)
 	}
+	handler := gziphandler.GzipHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000;")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Referrer-Policy", "origin")
+		w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline';")
+		w.Header().Set("X-Frame-Options", "DENY")
+		mux.ServeHTTP(w, r)
+	}))
 
-	mAddr := "localhost:" + strconv.Itoa(*port)
-	messengerServer := &http.Server{Addr: mAddr, Handler: mux}
+	httpAddr := ":" + strconv.Itoa(*httpPort)
+	httpHandler := handler
+	var httpsServer *http.Server
+
+	if *httpPort < 0 {
+		httpAddr = ":80"
+		httpHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
+		})
+
+		certManager := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: autocert.HostWhitelist(*domain),
+			Email:      *email,
+			Cache:      autocert.DirCache(*certCache),
+		}
+
+		httpsServer = &http.Server{
+			ReadTimeout:  readTimeout,
+			WriteTimeout: writeTimeout,
+			IdleTimeout:  idleTimeout,
+			ErrorLog:     infoLogger,
+			Addr:         ":443",
+			Handler:      handler,
+			TLSConfig: &tls.Config{
+				GetCertificate: certManager.GetCertificate,
+			},
+		}
+
+		go func() {
+			infoLogger.Printf("HTTPS server running at %s for domain %s", httpsServer.Addr, *domain)
+			if err = httpsServer.ListenAndServeTLS("", ""); err != nil {
+				errorLogger.Fatalln("failed to start HTTPS server:", err)
+			}
+		}()
+	}
+
+	httpServer := &http.Server{
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeout,
+		ErrorLog:     infoLogger,
+		Addr:         httpAddr,
+		Handler:      httpHandler,
+	}
 
 	go func() {
-		infoLogger.Printf("Server running at %s", mAddr)
-		if err = messengerServer.ListenAndServe(); err != nil {
-			errorLogger.Fatalln("failed to start server:", err)
+		infoLogger.Printf("HTTP server running at %s", httpServer.Addr)
+		if err = httpServer.ListenAndServe(); err != nil {
+			errorLogger.Fatalln("failed to start HTTP server:", err)
 		}
 	}()
 
 	// Wait for shutdown
 	<-shutdownSignals
 	infoLogger.Println("Waiting for connections before shutting down server.")
-	if err = messengerServer.Shutdown(context.Background()); err != nil {
-		errorLogger.Fatalln("failed to shutdown gracefully:", err)
+	if err = httpServer.Shutdown(context.Background()); err != nil {
+		errorLogger.Fatalln("failed to shutdown http server gracefully:", err)
+	}
+	if httpsServer != nil {
+		if err = httpsServer.Shutdown(context.Background()); err != nil {
+			errorLogger.Fatalln("failed to shutdown https server gracefully:", err)
+		}
 	}
 	infoLogger.Println("Server gracefully stopped.")
 }
